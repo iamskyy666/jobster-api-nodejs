6f7067c6d113988ee87f658058170a538f2596ac
Soumadip <39thdecember@gmail.com>
2026-06-18 14:46:20 +0530

    Fix: Password Gothcha 🔐

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,15 +7,13 @@ import UnauthenticatedError from "../errors/unauthenticated.js";
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-
   const token = user.createJWT();
-
   res.status(StatusCodes.CREATED).json({
     user: {
-      name: user.name,
       email: user.email,
       lastName: user.lastName,
       location: user.location,
+      name: user.name,
       token,
     },
   });
@@ -27,27 +25,22 @@ const login = async (req, res) => {
   if (!email || !password) {
     throw new BadRequestError("Please provide email and password");
   }
-
   const user = await User.findOne({ email });
-
   if (!user) {
     throw new UnauthenticatedError("Invalid Credentials");
   }
-
   const isPasswordCorrect = await user.comparePassword(password);
-
   if (!isPasswordCorrect) {
     throw new UnauthenticatedError("Invalid Credentials");
   }
-
+  // compare password
   const token = user.createJWT();
-
   res.status(StatusCodes.OK).json({
     user: {
-      name: user.name,
       email: user.email,
       lastName: user.lastName,
       location: user.location,
+      name: user.name,
       token,
     },
   });
@@ -55,43 +48,26 @@ const login = async (req, res) => {
 
 const updateUser = async (req, res) => {
   const { email, name, lastName, location } = req.body;
-
-  // Validate incoming data
   if (!email || !name || !lastName || !location) {
-    throw new BadRequestError("Please provide all values!");
+    throw new BadRequest("Please provide all values");
   }
+  const user = await User.findOne({ _id: req.user.userId });
 
-  // Find currently authenticated user
-  const user = await User.findById(req.user.userId);
-
-  if (!user) {
-    throw new UnauthenticatedError("User not found");
-  }
-
-  // Update fields
-  user.name = name;
   user.email = email;
+  user.name = name;
   user.lastName = lastName;
   user.location = location;
 
-  // Save updated document
   await user.save();
-
-  // Generate a fresh JWT
   const token = user.createJWT();
-
-  // User data to send back
-  const userResponse = {
-    name: user.name,
-    email: user.email,
-    lastName: user.lastName,
-    location: user.location,
-  };
-
   res.status(StatusCodes.OK).json({
-    user: userResponse,
-    token,
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+      token,
+    },
   });
 };
-
 export { register, login, updateUser };
--- models/User.model.js
+++ models/User.model.js
@@ -2,66 +2,73 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide name"],
-    minlength: 3,
-    maxlength: 50,
-  },
-  email: {
-    type: String,
-    required: [true, "Please provide email"],
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide name"],
+      minlength: 3,
+      maxlength: 50,
+      trim: true,
+    },
 
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide a valid email",
-    ],
+    email: {
+      type: String,
+      required: [true, "Please provide email"],
+      unique: true,
+      lowercase: true,
+      trim: true,
 
-    unique: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide a valid email",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+      minlength: 6,
+    },
+
+    lastName: {
+      type: String,
+      trim: true,
+      maxlength: 20,
+      default: "lastName",
+    },
+
+    location: {
+      type: String,
+      trim: true,
+      maxlength: 20,
+      default: "my city",
+    },
   },
-  password: {
-    type: String,
-    required: [true, "Please provide password"],
-    minlength: 6,
+  {
+    timestamps: true,
   },
-  lastName: {
-    type: String,
-    trim: true,
-    maxLength: 20,
-    default: "lastName",
-  },
-  location: {
-    type: String,
-    trim: true,
-    maxLength: 20,
-    default: "my city",
-  },
-});
+);
 
 UserSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
-
   const salt = await bcrypt.genSalt(10);
-
   this.password = await bcrypt.hash(this.password, salt);
 });
 
 UserSchema.methods.createJWT = function () {
   return jwt.sign(
-    {
-      userId: this._id,
-      name: this.name,
-    },
+    { userId: this._id, name: this.name },
     process.env.JWT_SECRET,
     {
-      expiresIn: process.env.JWT_LIFETIME || "30d",
+      expiresIn: process.env.JWT_LIFETIME,
     },
   );
 };
 
-UserSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
 };
 
 const User = mongoose.models.User || mongoose.model("User", UserSchema);
